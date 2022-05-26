b8b220029f216ca837341aba14f299ca48744d04
jKey Lu <jkeylu@gmail.com>
2022-05-26 21:27:18 +0800

    update type declaration

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,19 @@
 export interface Style {
-    NPM: 'NPM';
-    FMW: 'FMW';
+  NPM: 'NPM';
+  FMW: 'FMW';
 }
 
 export declare const STYLE: Style;
 
 export interface DrawOptions {
-    style?: keyof Style;
-    label?: string | ((node: any) => string | Promise<string>);
-    nodes?: string | ((node: any) => any[] | Promise<any[]>);
-    drawRootBranch?: boolean;
-    prefix?: string;
-    unicode?: boolean;
-    concurrency?: number;
+  style?: keyof Style;
+  label?: string | ((node: any) => string | Promise<string>);
+  nodes?: string | ((node: any) => any[] | Promise<any[]>);
+  drawRootBranch?: boolean;
+  prefix?: string;
+  unicode?: boolean;
+  concurrency?: number;
 }
 
-export function draw(node: any, options: DrawOptions): string;
-export function drawAsync(node: any, options: DrawOptions): Promise<string>;
+export declare function draw(node: any, options?: DrawOptions): string;
+export declare function drawAsync(node: any, options?: DrawOptions): Promise<string>;
